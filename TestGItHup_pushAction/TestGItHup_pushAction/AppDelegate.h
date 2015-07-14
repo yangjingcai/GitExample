e0d3ba754c8ef523dab07502ca17dde26fde241b
apple02 <apple02@hq006.local>
2015-07-14 14:06:04 +0800

    First insert a log

---
+++ TestGItHup_pushAction/TestGItHup_pushAction/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGItHup_pushAction
+//
+//  Created by apple02 on 15/7/14.
+//  Copyright (c) 2015年 apple02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGItHup_pushAction/TestGItHup_pushAction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGItHup_pushAction
+//
+//  Created by apple02 on 15/7/14.
+//  Copyright (c) 2015年 apple02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
