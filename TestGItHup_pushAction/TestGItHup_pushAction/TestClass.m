b8925af32a27520f175752db368d9fd48944e682
apple02 <apple02@hq006.local>
2015-07-14 14:11:43 +0800

    add testClass

---
+++ TestGItHup_pushAction/TestGItHup_pushAction/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  TestGItHup_pushAction
+//
+//  Created by apple02 on 15/7/14.
+//  Copyright (c) 2015年 apple02. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
