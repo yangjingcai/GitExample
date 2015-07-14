e36dbf90e414a1bb6d541a18da73fd8d6a0f8b30
apple02 <apple02@hq006.local>
2015-07-14 14:07:52 +0800

    add second log

--- TestGItHup_pushAction/TestGItHup_pushAction/ViewController.m
+++ TestGItHup_pushAction/TestGItHup_pushAction/ViewController.m
@@ -18,6 +18,7 @@
     [super viewDidLoad];
     NSLog(@"helo");
     // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"world");
 }
 
 - (void)didReceiveMemoryWarning {
